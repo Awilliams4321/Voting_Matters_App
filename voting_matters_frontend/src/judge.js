ac40436f3b0533f00e5f6cf4f5e87d3a45e6624b
Awilliams4321 <awilliams1234@live.com>
2020-09-23 12:51:03 -0600

    Adding an id property to my constructor method.

--- voting_matters_frontend/src/judge.js
+++ voting_matters_frontend/src/judge.js
@@ -1,6 +1,8 @@
 class Judge {
-    constructor(name, recommendation, details, judicial_district, image) {
+    constructor(id, name, recommendation, details, judicial_district, image) {
+        this.id = id
         this.name = name;
+        this.recommendation = recommendation;
         this.details = details;
         this.judicial_district = judicial_district;
         this.image = image;
