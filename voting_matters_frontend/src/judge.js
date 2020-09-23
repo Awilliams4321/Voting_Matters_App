2090fb701e18bd1a632d86fc8f0d58c82375e860
Awilliams4321 <awilliams1234@live.com>
2020-09-22 20:05:41 -0600

    Creating a JS Judge class and passing in my properties to the constructor method. Initializing those properties with 'this'.

---
+++ voting_matters_frontend/src/judge.js
@@ -0,0 +1,8 @@
+class Judge {
+    constructor(name, recommendation, details, judicial_district, image) {
+        this.name = name;
+        this.details = details;
+        this.judicial_district = judicial_district;
+        this.image = image;
+    }
+}
