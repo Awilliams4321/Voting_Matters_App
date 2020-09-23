087a4a5ce6ea7eb21d65551b7924d265a1a2c826
Awilliams4321 <awilliams1234@live.com>
2020-09-23 12:52:56 -0600

    Moving my index.js file into my src folder.

--- voting_matters_frontend/src/index.js
+++ voting_matters_frontend/src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    debugger;
+    fetchJudges();
 })
+
+const BASE_URL = "http://localhost:3000/"
+
+function fetchJudges() {
+    fetch(`${BASE_URL}judges`)
+    .then(resp => resp.json())
+    .then(judges => {
+       for (const judge of judges) {
+           console.log("rails obj", judge)
+           let j = new Judge(judge.id, judge.name, judge.recommendation, judge.details, judge.judicial_district, judge.image)
+           console.log("js obj", j)
+       }
+    })
+}
