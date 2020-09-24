b04ec51d2976ba54cfa952fba371cc73bd7ff06c
Awilliams4321 <awilliams1234@live.com>
2020-09-24 14:24:57 -0600

    Creating a function that creates a form form comment to be submitted. In this function, i am adding an event listener for submit and passing in commentSubmit as the callabck function.

--- voting_matters_frontend/src/index.js
+++ voting_matters_frontend/src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchJudges();
     fetchComments();
+    createCommentForm();
 })
 
 const BASE_URL = "http://localhost:3000/"
@@ -24,10 +25,28 @@ function fetchComments() {
     fetch(`${BASE_URL}comments`) 
     .then(resp => resp.json())
     .then(comments=> {
-        console.log("comments")
         for(const comment of comments) {
             let c = new Comment(comment.id, comment.note, comment.judge_id)
             c.renderComment(); 
         }
     })
+}
+
+function createCommentForm() {
+    let commentsForm = document.getElementById("comments-form")
+
+    commentsForm.innerHTML += 
+    `
+    <form> 
+        Note: <input type="text" id="note">
+        <input type="submit" value="Submit Note">
+    </form>
+    `
+    commentsForm.addEventListener("submit", commentSubmit)
+}
+
+function commentSubmit() {
+    event.preventDefault();
+    let note = document.getElementById("note").value
+    console.log(note)
 }
