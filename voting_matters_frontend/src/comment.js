9a40ffe8386700081902289e3abad231ff5c6f52
Awilliams4321 <awilliams1234@live.com>
2020-09-24 12:41:50 -0600

    Creating a method 'renderComment' to insert all existing comments into a displayed list.

--- voting_matters_frontend/src/comment.js
+++ voting_matters_frontend/src/comment.js
@@ -1,6 +1,15 @@
 class Comment {
+    static all_comments = []
     contructor(note, judge_id) {
         this.note = note;
         this.judge_id = judge_id;
+        Comment.all_comments.push(this)
     }
+
+    renderComment() {
+        let commentsDiv = document.getElementById("comments-container")
+        commentsDiv.innerHTML = (`<li>Comment: ${this.note}</li>`)
+    }
+
+  
 }
