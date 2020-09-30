ce9a5f0a7602bb4d1b025fb7af827442f7b3c37d
Awilliams4321 <awilliams1234@live.com>
2020-09-29 18:03:46 -0600

    Adding a class method(with static) to create an empty array to hold all Judges. Alsocreating a li element to append to the judgesDiv.

--- voting_matters_frontend/src/judge.js
+++ voting_matters_frontend/src/judge.js
@@ -1,10 +1,21 @@
 class Judge {
-    constructor(id, name, recommendation, details, judicial_district, image) {
+    static all_judges = []
+    constructor({id, name, recommendation, details, judicial_district, image}) {
         this.id = id
         this.name = name;
         this.recommendation = recommendation;
         this.details = details;
         this.judicial_district = judicial_district;
         this.image = image;
+        Judge.all_judges.push(this)
+    }
+
+    renderJudge() {
+        let judgesDiv = document.getElementById("judges-container")
+
+        let li = document.createElement("li")
+        li.innerText = this.name
+
+        judgesDiv.appendChild(li)
     }
 }
